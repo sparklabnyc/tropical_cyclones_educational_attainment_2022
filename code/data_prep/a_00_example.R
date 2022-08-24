5ef85cbeb7e9b5d658bd302b7d52cfcde018912e
rmp15 <r.parks15@imperial.ac.uk>
2022-08-24 10:38:35 -0400

    added file structure updates

--- code/data_prep/a_00_example.R
+++ code/data_prep/a_00_example.R
@@ -1,2 +1,9 @@
+rm(list=ls())
 
-data_educational_attainment_full_raw = readr::read_csv()
+# Declare root directory, folder locations and load essential stuff
+project.folder = paste0(print(here::here()),'/')
+
+
+# load the csv
+# TO DO
+data_educational_attainment_full_raw = readr::read_csv(educational_attainment_full_local_file)
