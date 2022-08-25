60e239429e5355c8ae0888f7385681a2d4c0e996
Gabriella Meltzer <gm3085@cumc.columbia.edu>
2022-08-24 21:00:10 -0400

    Gabriella installed and loaded dplyr; then checked the unique values of fips

--- code/data_prep/a_01_prepare_educational_attainment_data.R
+++ code/data_prep/a_01_prepare_educational_attainment_data.R
@@ -9,3 +9,7 @@ source(paste0(functions.folder,'functions.R'))
 
 # load the local educational attainment full csv 
 data_educational_attainment_full_raw = readr::read_csv(educational_attainment_full_local_file)
+
+# determine the unique values of fips 
+data_educational_attainment_full_raw %>% pull(fips) %>% unique()
+  
