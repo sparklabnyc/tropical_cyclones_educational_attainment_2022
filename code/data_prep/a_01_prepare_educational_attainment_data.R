6d51401167873948d05d87efd2193c63ba111950
rmp15 <r.parks15@imperial.ac.uk>
2022-08-31 11:06:10 -0400

    added example code for how to process by state

--- code/data_prep/a_01_prepare_educational_attainment_data.R
+++ code/data_prep/a_01_prepare_educational_attainment_data.R
@@ -8,8 +8,24 @@ source(paste0(objects.folder,'objects.R'))
 source(paste0(functions.folder,'functions.R'))
 
 # load the local educational attainment full csv 
-data_educational_attainment_full_raw = readr::read_csv(educational_attainment_full_local_file)
+# Robbie: Research how to specify that the column for state FIPS is a character when loaded
+data_educational_attainment_full_raw = readr::read_csv(educational_attainment_full_local_file) 
 
-# determine the unique values of fips 
-data_educational_attainment_full_raw %>% pull(fips) %>% unique()
+# determine the unique values of fips (Robbie: Check if state FIPS)
+states = data_educational_attainment_full_raw %>% pull(fips) %>% unique() %>% arrange()
+
+# loop through each state and save
+for(state_current in states){
+  # filter only one state
+  data_educational_attainment_full_raw_state = data_educational_attainment_full_raw %>%
+    dplyr::filter(fips==state_current)
+  
+  # save as RDS file
+  saveRDS(data_educational_attainment_full_raw_state, 
+          paste0(processed.educational.attainment.folder,state_current,'_educational_attainment.rds'))
+}
+
+# Robbie: after this (just to check so not to keep code), test load the RDS files with readRDS
+# Robbie: also check that each of the output state files is less than 100MB
+# test = readRDS(paste0(processed.educational.attainment.folder,'01_educational_attainment.rds'))
   
