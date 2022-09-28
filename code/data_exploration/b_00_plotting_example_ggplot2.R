a55c5379d44a4cba4d4bdf6155e6f324dfedc4a0
Gabriella Meltzer <gm3085@cumc.columbia.edu>
2022-09-28 10:49:54 -0400

    Created plots for all states for math and reading

--- code/data_exploration/b_00_plotting_example_ggplot2.R
+++ code/data_exploration/b_00_plotting_example_ggplot2.R
@@ -7,26 +7,32 @@ source(paste0(file.locations.folder,'file_locations.R'))
 source(paste0(objects.folder,'objects.R'))
 source(paste0(functions.folder,'functions.R'))
 
+
 # load the local educational attainment for single state
-state_selected = 36
-data = readRDS(paste0(processed.educational.attainment.folder,state_selected,
-                              '_educational_attainment_gcs.rds'))
-
-# prepare data for plot
-data_to_plot = data %>%
-  dplyr::filter(subject=='mth') # or 'rla'
-
-# create plot
-plot = ggplot(data_to_plot) +
-  geom_line(aes(x=year,y=gcs_mn_all,color=as.factor(sedacounty))) + 
-  facet_wrap(~grade) +
-  xlab('Year') + ylab('Standardized score') +
-  guides(color="none")
+# state_selected = 36
+for (state_selected in states_included) {
+  for (subject_selected in subjects_included) {
+    data = readRDS(paste0(processed.educational.attainment.folder,state_selected,
+                          '_educational_attainment_gcs.rds'))
+    data_to_plot = data %>%
+      dplyr::filter(subject==subject_selected) 
+    
+    # create plot
+    plot = ggplot(data_to_plot) +
+      geom_line(aes(x=year,y=gcs_mn_all,color=as.factor(sedacounty))) + 
+      facet_wrap(~grade) +
+      xlab('Year') + ylab('Standardized score') +
+      ggtitle(paste0(state_selected," ", subject_selected)) +
+      guides(color="none")
+    
+    # create plot
+    plot
+    
+    # output as pdf
+    pdf(paste0(plot.output.folder,state_selected, '_',subject_selected,'_plot_over_time.pdf'),height=0,width=0,paper='a4r')
+    print(plot)
+    dev.off()
+  }
   
-# create plot
-plot
+}
 
-# output as pdf
-pdf(paste0(plot.output.folder,state_selected,'_plot_over_time.pdf'),height=0,width=0,paper='a4r')
-plot
-dev.off()
