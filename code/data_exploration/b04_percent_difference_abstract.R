b4de5b556f015ef40c3d237dbe0d6c47beded218
Gabriella Meltzer <gm3085@cumc.columbia.edu>
2022-12-08 15:13:22 -0500

    Created R script calculating percent difference for abstract; incorporated information into abstract and cut words.

---
+++ code/data_exploration/b04_percent_difference_abstract.R
@@ -0,0 +1,12 @@
+rm(list=ls())
+
+# Declare root directory, folder locations, local file locations, objects, and functions
+project.folder = paste0(print(here::here()),'/')
+source(paste0(project.folder,'create_folder_structure.R'))
+source(paste0(file.locations.folder,'file_locations.R'))
+source(paste0(objects.folder,'objects.R'))
+source(paste0(functions.folder,'functions.R'))
+
+library(dplyr)
+
+math_DID <- math_DID %>% group_by(state,grade) %>% summarise(grade_avg=mean(gcs_mn_all))
