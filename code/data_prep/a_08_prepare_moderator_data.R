0ce49dfc4b6fc19dcf79610a1c8a5d5c3132778e
Gabriella Meltzer <gm3085@cumc.columbia.edu>
2024-02-17 21:41:53 -0500

    Edited manuscript based on feedback from coauthors and edited manuscript and supplement to reflect lagged hurricane exposure

--- code/data_prep/a_08_prepare_moderator_data.R
+++ code/data_prep/a_08_prepare_moderator_data.R
@@ -126,3 +126,55 @@ rla_DID <- rla_DID %>% mutate(sesall_tert = as_factor(sesall_tert))
 saveRDS(math_DID, paste0(prepared.math.DID.folder, "math_DID_cyclones.rds"))
 saveRDS(rla_DID, paste0(prepared.rla.DID.folder, "rla_DID_cyclones.rds"))
 
+
+#Load lagged hurricane data sets
+math_DID <- readRDS(paste0(prepared.math.DID.folder, "math_DID_hurricanes_lagged.rds"))
+rla_DID <- readRDS(paste0(prepared.rla.DID.folder, "rla_DID_hurricanes_lagged.rds"))
+
+#Create tertile variable for percent Black, Hispanic, and Native American students in the cohort in math data frame
+math_DID <- math_DID %>% mutate(minority = pernam + perhsp + perblk) 
+math_DID <- math_DID %>% group_by(year) %>% mutate(minority_tert = ntile(minority, 3)) 
+math_DID <- math_DID %>% mutate(minority_tert = as_factor(minority_tert))
+
+#Create tertile variable for percent Black, Hispanic, and Native American students in the cohort in RLA data frame
+rla_DID <- rla_DID %>% mutate(minority = pernam + perhsp + perblk) 
+rla_DID <- rla_DID %>% group_by(year) %>% mutate(minority_tert = ntile(minority, 3))
+rla_DID <- rla_DID %>% mutate(minority_tert = as_factor(minority_tert)) 
+
+#Create tertile variable for county poverty in math data frame
+math_DID <- math_DID %>% group_by(year) %>% mutate(poverty_tert = ntile(povertyall, 3)) 
+math_DID <- math_DID %>% mutate(poverty_tert = as_factor(poverty_tert))
+
+#Create tertile variable for county poverty in RLA data frame
+rla_DID <- rla_DID %>% group_by(year) %>% mutate(poverty_tert = ntile(povertyall, 3)) 
+rla_DID <- rla_DID %>% mutate(poverty_tert = as_factor(poverty_tert))
+
+#Create tertile variable for percent special education in county in RLA data frame
+rla_DID <- rla_DID %>% group_by(year) %>% mutate(perspeced_tert = ntile(perspeced, 3)) 
+rla_DID <- rla_DID %>% mutate(perspeced_tert = as_factor(perspeced_tert))
+
+#Create tertile variable for percent special education in county in math data frame
+math_DID <- math_DID %>% group_by(year) %>% mutate(perspeced_tert = ntile(perspeced, 3)) 
+math_DID <- math_DID %>% mutate(perspeced_tert = as_factor(perspeced_tert))
+
+#Creating reverse tertile variable for percent special education in county in RLA data frame
+rla_DID <- rla_DID %>% mutate(perspeced_rec = 1 - perspeced)
+rla_DID <- rla_DID %>% group_by(year) %>% mutate(perspeced_rec_tert = ntile(perspeced_rec, 3)) 
+rla_DID <- rla_DID %>% mutate(perspeced_rec_tert = as_factor(perspeced_rec_tert))
+
+#Creating reverse tertile variable for percent special education in county in math data frame
+math_DID <- math_DID %>% mutate(perspeced_rec = 1 - perspeced)
+math_DID <- math_DID %>% group_by(year) %>% mutate(perspeced_rec_tert = ntile(perspeced_rec, 3)) 
+math_DID <- math_DID %>% mutate(perspeced_rec_tert = as_factor(perspeced_rec_tert))
+
+#Create tertile variable for county SES composite variable in math data frame
+math_DID <- math_DID %>% group_by(year) %>% mutate(sesall_tert = ntile(sesall, 3)) 
+math_DID <- math_DID %>% mutate(sesall_tert = as_factor(sesall_tert))
+
+#Create tertile variable for county SES composite variable in math data frame
+rla_DID <- rla_DID %>% group_by(year) %>% mutate(sesall_tert = ntile(sesall, 3)) 
+rla_DID <- rla_DID %>% mutate(sesall_tert = as_factor(sesall_tert))
+
+#Save data frames as RDS files
+saveRDS(math_DID, paste0(prepared.math.DID.folder, "math_DID_hurricanes_lagged.rds"))
+saveRDS(rla_DID, paste0(prepared.rla.DID.folder, "rla_DID_hurricanes_lagged.rds"))
